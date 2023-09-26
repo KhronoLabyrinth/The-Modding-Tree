f5c37a74d317c4a6e3a6d2cd638e7744c2460134
KhronoLabyrinth <km698@616.sb>
2023-09-26 22:18:20 +0900

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Chrome Tree",
+	id: "idkhowtowriteanid",
+	author: "kl7297",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
